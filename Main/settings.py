cac4586112b9f008846521cb04e6a759b05568e7
whitebear19 <bendan198242@gmail.com>
2021-01-09 15:22:43 +0900

    update

--- Main/settings.py
+++ Main/settings.py
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),     
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ticket',
-        'USER': 'root',
-        'PASSWORD': "",
-        'HOST': "",
-        'PORT': "",   
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),     
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'ticket',
+        # 'USER': 'root',
+        # 'PASSWORD': "",
+        # 'HOST': "",
+        # 'PORT': "",   
     }
 }
 
